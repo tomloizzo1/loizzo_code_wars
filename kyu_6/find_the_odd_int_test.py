62869847eaf1226cdab7ae401277bf7333c4dcc7
Tom Loizzo <tom.loizzo1@accesscfa.com>
2024-04-16 17:08:26 -0400

    Corrected test files to utilize assertEquals in kyu_6 test files
    Completed find_the_odd_int.py

--- kyu_6/find_the_odd_int_test.py
+++ kyu_6/find_the_odd_int_test.py
@@ -1,14 +1,15 @@
 import unittest
+import operator
 from find_the_odd_int import find_it
 
 
 class TestOddIntFinder(unittest.TestCase):
     def test_find_it(self):
         # Test to see which number appears an odd number of time in a seqwuence
-        self.assert_equals(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]), 5)
-        self.assert_equals(find_it([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]), -1);
-        self.assert_equals(find_it([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]), 5);
-        self.assert_equals(find_it([10]), 10);
-        self.assert_equals(find_it([10, 10, 10]), 10);
-        self.assert_equals(find_it([1, 1, 1, 1, 1, 1, 10, 1, 1, 1,
-        self.assert_equals(find_it([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]), 1);
+        self.assertEquals(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]), 5)
+        self.assertEquals(find_it([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]), -1)
+        self.assertEquals(find_it([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]), 5)
+        self.assertEquals(find_it([10]), 10)
+        self.assertEquals(find_it([10, 10, 10]), 10)
+        self.assertEquals(find_it([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]), 10)
+        self.assertEquals(find_it([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]), 1)
--- kyu_6/playing_with_digits_test.py
+++ kyu_6/playing_with_digits_test.py
@@ -5,9 +5,9 @@ from playing_with_digits import dig_pow
 class TestCircleArea(unittest.TestCase):
     def test_case(self):
         # Tests areas when radius >= 0
-        self.assertAlmostEqual(dig_pow(89,1), 1)
-        self.assert_equals(dig_pow(92, 1), -1)
-        self.assert_equals(dig_pow(46288, 3), 51)
-        # self.assert_equals(dig_pow(41, 5), 25)
-        # self.assert_equals(dig_pow(114, 3), 9)
-        # self.assert_equals(dig_pow(8, 3), 64)
+        self.assertEquals(dig_pow(89,1), 1)
+        self.assertEquals(dig_pow(92, 1), -1)
+        self.assertEquals(dig_pow(46288, 3), 51)
+        self.assertEquals(dig_pow(41, 5), 25)
+        self.assertEquals(dig_pow(114, 3), 9)
+        self.assertEquals(dig_pow(8, 3), 64)
