24e7eb2696a9e532b3e562c3e9270a6f918a9398
Tom Loizzo <tom.loizzo1@accesscfa.com>
2024-04-15 22:04:55 -0400

    Completed playing_with_digits.py

---
+++ kyu_6/playing_with_digits.py
@@ -0,0 +1,33 @@
+"""
+Some numbers have funny properties. For example:
+
+89 --> 8¹ + 9² = 89 * 1
+695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
+
+In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be strictly positive integers.
+
+Examples:
+n = 89; p = 1 ---> 1 since 8¹ + 9² = 89 = 89 * 1
+
+n = 92; p = 1 ---> -1 since there is no k such that 9¹ + 2² equals 92 * k
+"""
+
+
+def dig_pow(n, p):
+    sum = 0
+    for i in str(n):
+        powered = int(i) ** p
+        sum += powered
+        p += 1
+    if sum % n == 0:
+        return sum / n
+    else:
+        return - 1
+
+if __name__ == '__main__':
+    print(dig_pow(89,1))
