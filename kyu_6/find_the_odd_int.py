b82bcb20249129739cc7d3ddbc47903e4a4fc97d
Tom Loizzo <tom.loizzo1@accesscfa.com>
2024-04-16 17:10:08 -0400

    Updated find_the_odd_int.py to remove unnecessary import statements

--- kyu_6/find_the_odd_int.py
+++ kyu_6/find_the_odd_int.py
@@ -12,14 +12,12 @@ Examples
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 """
-import operator
-
 def find_it(seq):
     """
     :param seq: Takes a sequence of integers with one integer repeating an odd number of times
     :return: returns the integer repeating an odd number of times
     """
     for i in seq:
-        if operator.countOf(seq, i) % 2 >0:
+        if seq.count(i) % 2:
             return i
 
